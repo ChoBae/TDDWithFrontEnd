8b85e57004cc5c706059fb427217cdd7b1948283
Cho Sung Bae <101175828+ChoBaeKR@users.noreply.github.com>
2023-08-07 16:43:07 +0900

    add: color test

--- ui-test/src/App.test.js
+++ ui-test/src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("빨간색 버튼이 있는지 확인", () => {
   render(<App />);
-  const linkElement = screen.getByRole("link", { name: /learn react/i });
-  expect(linkElement).toBeInTheDocument();
+  const buttonElement = screen.getByRole("button", { name: "파랑색으로 변경" });
+  // expect(buttonElement).toBeInTheDocument();
+
+  expect(buttonElement).toBeInTheDocument();
 });
