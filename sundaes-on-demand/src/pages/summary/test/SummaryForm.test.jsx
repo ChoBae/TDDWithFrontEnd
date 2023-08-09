c3eabbe5fc6296574b69357deb64a31ce44d80ae
Cho Sung Bae <101175828+ChoBaeKR@users.noreply.github.com>
2023-08-09 15:53:52 +0900

    Replace fireEvent with userEvent

--- sundaes-on-demand/src/pages/summary/test/SummaryForm.test.jsx
+++ sundaes-on-demand/src/pages/summary/test/SummaryForm.test.jsx
@@ -1,10 +1,11 @@
 import SummaryForm from "../SummaryForm";
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 test("Initial conditions", () => {
   render(<SummaryForm />);
   const checkboxEl = screen.getByRole("checkbox", {
-    name: /terms and conditions/i,
+    name: /I agree to terms and conditions/i,
   });
   // 초기 체크박스가 체크되어 있지 않아야 한다.
   expect(checkboxEl).not.toBeChecked();
@@ -15,18 +16,19 @@ test("Initial conditions", () => {
   expect(buttonEl).toBeDisabled();
 });
 
-test("Checkbox enables button on first click and disables on second click", () => {
+test("Checkbox enables button on first click and disables on second click", async () => {
   render(<SummaryForm />);
+  const user = userEvent.setup();
   const checkboxEl = screen.getByRole("checkbox", {
     name: /terms and conditions/i,
   });
-  fireEvent.click(checkboxEl);
-  // 체크박스를 클릭하면 버튼이 활성화되어야함
   const buttonEl = screen.getByRole("button", {
     name: /confirm order/i,
   });
+  await user.click(checkboxEl);
+  // 체크박스를 클릭하면 버튼이 활성화되어야함
   expect(buttonEl).toBeEnabled();
   // 다시 체크박스를 클릭하면 버튼이 비활성화되어야함
-  fireEvent.click(checkboxEl);
+  await user.click(checkboxEl);
   expect(buttonEl).toBeDisabled();
 });
