2c01921a65bc487f01014aa4e26490130addfc95
Cho Sung Bae <101175828+ChoBaeKR@users.noreply.github.com>
2023-08-15 16:23:52 +0900

    Test and code toppings options from server

--- sundaes-on-demand/src/pages/entry/tests/Options.test.js
+++ sundaes-on-demand/src/pages/entry/tests/Options.test.js
@@ -2,11 +2,18 @@ import { screen, render } from "@testing-library/react";
 import Options from "../Options";
 
 test("displays image for each scoop option from server", async () => {
-    render(<Options optionType="scoops" />);
-    // 서버에서 사진이 로드되는지 확인한다.
-    const scoopImages = await screen.findAllByRole("img", { name: /scoop$/i });
-    expect(scoopImages).toHaveLength(2);
-    const altText = scoopImages.map((element) => element.alt);
-    expect(altText).toEqual(["Chocolate scoop", "Vanilla scoop"]);
-    
+  render(<Options optionType="scoops" />);
+  // 서버에서 사진이 로드되는지 확인한다.
+  const scoopImages = await screen.findAllByRole("img", { name: /scoop$/i });
+  expect(scoopImages).toHaveLength(2);
+  const altText = scoopImages.map((element) => element.alt);
+  expect(altText).toEqual(["Chocolate scoop", "Vanilla scoop"]);
+});
+
+test("displays image for each toppings option from server", async () => {
+    render(<Options optionType="toppings" />);
+    const toppingImages = await screen.findAllByRole("img", { name: /topping$/i });
+    expect(toppingImages).toHaveLength(3);
+    const altText = toppingImages.map((element) => element.alt);
+    expect(altText).toEqual(["Cherries topping", "M&Ms topping", "Hot fudge topping"]);
 });
