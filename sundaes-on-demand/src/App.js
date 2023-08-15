56f8edcf05bdc088298603219e30fbf2016014bf
Cho Sung Bae <101175828+ChoBaeKR@users.noreply.github.com>
2023-08-15 16:59:51 +0900

    Test and code alert for server error response

---
+++ sundaes-on-demand/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import SummaryForm from "./pages/summary/SummaryForm";
+
+function App() {
+  return (
+    <div className="App">
+      <SummaryForm></SummaryForm>
+    </div>
+  );
+}
+
+export default App;
--- sundaes-on-demand/src/pages/entry/Options.jsx
+++ sundaes-on-demand/src/pages/entry/Options.jsx
@@ -3,8 +3,10 @@ import { useEffect, useState } from "react";
 import ScoopOption from "./ScoopOption";
 import ToppingOption from "./ToppingOption";
 import Row from "react-bootstrap/Row";
+import AlertBanner from "../common/AlertBanner";
 export default function Options({ optionType }) {
   const [items, setItems] = useState([]);
+  const [error, setError] = useState(false);
   useEffect(() => {
     axios
       .get(`http://localhost:3030/${optionType}`)
@@ -12,11 +14,13 @@ export default function Options({ optionType }) {
         setItems(response.data);
       })
       .catch((error) => {
-        // TODO: handle error response
-        console.log(error);
+        setError(true);
       });
   }, [optionType]);
 
+  if (error) {
+    return <AlertBanner />;
+  }
   const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption;
   const optionItems = items.map((item) => (
     <ItemComponent
